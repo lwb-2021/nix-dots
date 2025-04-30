2740f4dd12b017adf9c6f12e54cbb008a8990695
lwb <lwb-2021@qq.com>
2025-04-30 21:50:21 +0800

    use build flags to java to improve performance

--- home/programs/minecraft.nix
+++ home/programs/minecraft.nix
@@ -1,11 +1,19 @@
 { config, pkgs, ... }: {
-    home.packages = with pkgs;[(prismlauncher.override {
-      # Add binary required by some mod
-      additionalPrograms = [ ffmpeg ];
+    let pkgs-flags = import <nixpkgs> {
+        localSystem = {
+            gcc.arch = "alderlake";
+            gcc.tune = "alderlake";
+            system = "x86_64-linux";
+        };
+    };
+    in
+        home.packages = with pkgs;[(prismlauncher.override {
+            # Add binary required by some mod
+            additionalPrograms = [ ffmpeg ];
   
-      # Change Java runtimes available to Prism Launcher
-      jdks = [ pkgs.jdk21 pkgs.jdk17 pkgs.jdk8 ];
-    })
-      gamemode
-    ];
+            # Change Java runtimes available to Prism Launcher
+            jdks = with pkgs-flags; [ jdk21 jdk17 jdk8 ];
+        })
+            gamemode
+        ];
 }
