299e4b5bc067a1594cf031a7f2d7dd07e5be429e
lwb <lwb-2021@qq.com>
2025-04-30 21:56:52 +0800

    fix syntax error

--- home/programs/minecraft.nix
+++ home/programs/minecraft.nix
@@ -1,19 +1,20 @@
-{ config, pkgs, ... }: {
-    let pkgs-flags = import <nixpkgs> {
-        localSystem = {
-            gcc.arch = "alderlake";
-            gcc.tune = "alderlake";
-            system = "x86_64-linux";
-        };
+
+let pkgs-flags = import <nixpkgs> {
+    localSystem = {
+        gcc.arch = "alderlake";
+        gcc.tune = "alderlake";
+        system = "x86_64-linux";
     };
-    in
-        home.packages = with pkgs;[(prismlauncher.override {
+};
+in
+{ config, pkgs, ... }: {
+    home.packages = [(pkgs.prismlauncher.override {
             # Add binary required by some mod
-            additionalPrograms = [ ffmpeg ];
-  
+            additionalPrograms = with pkgs;[ ffmpeg ];
+
             # Change Java runtimes available to Prism Launcher
-            jdks = with pkgs-flags; [ jdk21 jdk17 jdk8 ];
+            jdks = with pkgs-flags;[ jdk21 jdk17 jdk8 ];
         })
-            gamemode
-        ];
+        pkgs.gamemode
+    ];
 }
