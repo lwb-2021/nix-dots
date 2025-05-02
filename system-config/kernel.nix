dfaeb2d3b40029982073930f4a8515697ef6992a
lwb <lwb-2021@qq.com>
2025-05-02 08:19:09 +0800

    add nvidia kernel module

--- system-config/kernel.nix
+++ system-config/kernel.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
     boot = {
         kernelPackages = pkgs.linuxKernel.kernels.linux_zen;
+        extraModulePackages = with config.boot.kernelPackages; [ nvidiaPackages.stable ];
     }; 
-    hardware.nvidia.package = lib.mkForce pkgs.linuxKernel.packages.linux_zen.nvidia_x11_latest_open;
 
 }
