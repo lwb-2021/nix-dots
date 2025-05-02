5c248e83a0494200a30c9113602853844685b328
lwb <lwb-2021@qq.com>
2025-05-02 20:26:07 +0800

    fixup! fixup! fixup! fixup! fixup! use ags instead of waybar

--- home/programs/desktop/ags/flake-module.nix
+++ home/programs/desktop/ags/flake-module.nix
@@ -1,10 +1,9 @@
 { config, lib, pkgs, inputs, ... }: {
-    home-manager.users.username = {
+    home-manager.users.lwb = {
         imports = [ inputs.ags.homeManagerModules.default ];
         programs.ags = {
             enable = true;
         };
-
+        home.stateVersion = "25.05";
     };
-    home.stateVersion = "25.05";
 }
