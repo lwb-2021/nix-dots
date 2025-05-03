6286b06e4400debadbded2c542c4ef6e83c65c63
lwb <lwb-2021@qq.com>
2025-05-03 11:05:05 +0800

    add vscodium

---
+++ home/programs/code.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, ... }: {
+    programs.vscode = {
+        enable = true;
+        package = pkgs.vscodium;
+        profiles.default = {
+            extensions = with pkgs.vscode-extensions;[
+                bbenoist.nix
+            ];
+        };
+    };
+
+}
