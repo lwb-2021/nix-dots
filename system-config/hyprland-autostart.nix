63fecaeababc0255f398b7a64f2c8c5fcfb9e14f
lwb <lwb-2021@qq.com>
2025-05-06 14:58:15 +0800

    autologin

--- system-config/hyprland-autostart.nix
+++ system-config/hyprland-autostart.nix
@@ -1,4 +1,6 @@
 { config, lib, pkgs, ... }: {
+
+    services.getty.autologinUser = "lwb"; 
     programs.bash.loginShellInit = ''
         if [ -z "''${DISPLAY}" ] && [ "''${XDG_VTNR}" -eq 1 ]; then
             exec Hyprland
