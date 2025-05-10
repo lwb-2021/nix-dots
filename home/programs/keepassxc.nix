2d426fcf5a0e81aaf2b146aa8ee4a80ed0e6e048
lwb <lwb-2021@qq.com>
2025-05-10 13:55:54 +0800

    add keepassxc config

--- home/programs/keepassxc.nix
+++ home/programs/keepassxc.nix
@@ -1,7 +1,25 @@
 { config, lib, pkgs, ... }: {
     programs.keepassxc = {
         enable = true;
+        settings = {
+            General = {
+                ConfigVersion="2";
+                MinimizeAfterUnlock=true;
 
+            };
+            Browser = {
+                Enabled = true;
+            };
+            GUI = {
+                ApplicationTheme="classic";
+                CompactMode=true;
+                MinimizeOnClose=true;
+                MinimizeOnStartup=true;
+                MinimizeToTray=true;
+                ShowTrayIcon=true;
+                TrayIconAppearance="colorful";
+            };
+        };
     };
 
 }
