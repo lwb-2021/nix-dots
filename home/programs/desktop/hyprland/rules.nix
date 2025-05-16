f1b1f922294face19d6b31430385239b0f0bbde7
lwb <lwb-2021@qq.com>
2025-05-16 20:47:20 +0800

    update window rule

--- home/programs/desktop/hyprland/rules.nix
+++ home/programs/desktop/hyprland/rules.nix
@@ -5,6 +5,9 @@
                 "float, title:.*正在.*"
                 #"size 400 200, title:.*正在.*"
                 "move 100%-w-20 80, title:.*正在.*"
+
+                "float, class:org.kde.kdeconnect.handler"
+                "center, class:org.kde.kdeconnect.handler"
             ];
         };
     };
