eccde3549f19ad8221223106d30402757df5ed76
lwb <lwb-2021@qq.com>
2025-05-16 19:18:44 +0800

    change window rule

--- home/programs/desktop/hyprland/rules.nix
+++ home/programs/desktop/hyprland/rules.nix
@@ -4,7 +4,7 @@
             windowrule = [
                 "float, title:.*正在.*"
                 #"size 400 200, title:.*正在.*"
-                "move 100%-w-20 20, title:.*正在.*"
+                "move 100%-w-20 80, title:.*正在.*"
             ];
         };
     };
