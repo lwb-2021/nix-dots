0800b167de78f3db5bffd31300b28235c73e5344
lwb <lwb-2021@qq.com>
2025-05-25 07:38:27 +0800

    enable cuda

--- system-config/nvidia.nix
+++ system-config/nvidia.nix
@@ -1,29 +1,33 @@
 { config, lib, pkgs, ... }:
 {
-    hardware.graphics = {
+  hardware.graphics = {
+    enable = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = true; # false
+
+    powerManagement.finegrained = true; # false
+
+    open = true;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      offload = {
         enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
     };
-    services.xserver.videoDrivers = ["nvidia"];
-        hardware.nvidia = {
-        modesetting.enable = true;
+  };
 
-        powerManagement.enable = true; # false
-
-        powerManagement.finegrained = true; # false
-
-        open = true;
-
-        nvidiaSettings = true;
-
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
-        prime = {
-            offload = {
-                enable = true;
-                enableOffloadCmd = true;
-            };
-            intelBusId = "PCI:0:2:0";
-            nvidiaBusId = "PCI:1:0:0";
-        };
-    };
+  nixpkgs.config = {
+    cudaSupport = true;
+  };
 
 }
