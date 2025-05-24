ccaa867080cee83a3a5bc680bbf506b6118fd0dc
lwb <lwb-2021@qq.com>
2025-05-24 16:16:34 +0800

    Revert "use fish in hyprland autostart"
    This reverts commit 04a53c1c6b097f9ddd53a52b816c80c2165cf4e0.

--- system-config/hyprland-autostart.nix
+++ system-config/hyprland-autostart.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }: {
 
     services.getty.autologinUser = "lwb"; 
-    programs.fish.loginShellInit = ''
-        bash -c 'if [ -z "''${DISPLAY}" ] && [ "''${XDG_VTNR}" -eq 1 ]; then
+    programs.bash.loginShellInit = ''
+        if [ -z "''${DISPLAY}" ] && [ "''${XDG_VTNR}" -eq 1 ]; then
             exec Hyprland
-        fi'
+        fi
     '';
 }
