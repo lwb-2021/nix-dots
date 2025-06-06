e3cb6af449985fe744d7d6ab9f6ecfa3e697372c
lwb <lwb-2021@qq.com>
2025-06-06 21:59:17 +0800

    enable electron wayland support

--- home/programs/desktop/hypr/hyprland/environment.nix
+++ home/programs/desktop/hypr/hyprland/environment.nix
@@ -1,11 +1,14 @@
 { config, pkgs, lib, ... }: {
-    wayland.windowManager.hyprland = {
-        systemd = {
-            variables = ["--all"];
-        };
-        settings = {
-            env = [];
-        };
+  wayland.windowManager.hyprland = {
+    systemd = {
+      variables = ["--all"];
     };
+    settings = {
+      env = [];
+    };
+  };
+  home.sessionVariables = {
+    NIXOS_OZONE_WL = 1;
+  };
 
 }
