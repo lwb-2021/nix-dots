83966f5b8b73503120283dbede8965d6f4423f4e
lwb <lwb-2021@qq.com>
2025-06-07 18:20:15 +0800

    add sjtu mirror

--- configuration.nix
+++ configuration.nix
@@ -145,6 +145,7 @@
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
     ];
