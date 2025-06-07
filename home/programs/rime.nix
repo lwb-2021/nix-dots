6d023ba24422d8dbeed38c20db7aafcef0811ff2
lwb <lwb-2021@qq.com>
2025-06-07 10:13:20 +0800

    change fcitx5 keymap

--- home/programs/rime.nix
+++ home/programs/rime.nix
@@ -1,24 +1,30 @@
 { config, pkgs, ... }: { 
-    i18n = { 
-        inputMethod = { 
-            enable = true; 
-            type = "fcitx5"; 
-            fcitx5 = { 
-                addons = with pkgs;[
-                    fcitx5-gtk
-                    fcitx5-rime
-                    fcitx5-mellow-themes
-                ]; 
-                waylandFrontend = true; 
-                settings = { 
-                    addons = { 
-                        classicui.globalSection = {
-                            Theme = "kwinblur-mellow-graphite-dark";
-                            # DarkTheme = "plasma"; 
-                        };
-                    };
-                };
+  i18n = { 
+    inputMethod = { 
+      enable = true; 
+      type = "fcitx5"; 
+      fcitx5 = { 
+        addons = with pkgs;[
+          fcitx5-gtk
+          fcitx5-rime
+          fcitx5-mellow-themes
+        ]; 
+        waylandFrontend = true; 
+        settings = {
+          globalOptions = {
+            Hotkey = {
+              EnumerateWithTriggerKeys = true;
+              "TriggerKeys/0" = "Alt+Shift+Shift_R";
             };
+          };
+          addons = { 
+            classicui.globalSection = {
+              Theme = "kwinblur-mellow-graphite-dark";
+              # DarkTheme = "plasma"; 
+            };
+          };
         };
+      };
     };
+  };
 }
