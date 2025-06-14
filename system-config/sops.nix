da677d6df4b256af3df0c473a5939da4039943cf
lwb <lwb-2021@qq.com>
2025-06-14 17:10:14 +0800

    enable sops

--- system-config/sops.nix
+++ system-config/sops.nix
@@ -1,3 +1,9 @@
 { config, lib, pkgs, ... }: {
-
+  # sops.age.keyFile = "/nix/persist/var/lib/sops-nix/key.txt";
+  # Or
+  # fileSystems."/etc/ssh".neededForBoot = true;
+  environment.systemPackages = [ pkgs.sops ];
+  sops.secrets = {
+    
+  };
 }
