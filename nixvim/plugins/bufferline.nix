5f6fee5afdea2da25fccc0efaf7f38d085e3e3ba
lwb <lwb-2021@qq.com>
2025-06-14 16:26:15 +0800

    nixvim:configure bufferline

--- nixvim/plugins/bufferline.nix
+++ nixvim/plugins/bufferline.nix
@@ -1,3 +1,17 @@
 {
-    enable = true;
+  enable = true;
+  settings = {
+    options = {
+      diagnostics = "nvim_lsp";
+      offsets = [
+        {
+          filetype = "NvimTree";
+          text = "File Explorer";
+          highlight = "Directory";
+          text_align = "left";
+        }
+      ];
+    };
+  };
+
 }
