0b5aa115518c5000502b292dae2241edf4402a7c
lwb <lwb-2021@qq.com>
2025-06-27 17:12:48 +0800

    add python packages

--- home/programs/development/python.nix
+++ home/programs/development/python.nix
@@ -1,12 +1,18 @@
 { config, lib, pkgs, ... }: {
-    home.packages = with pkgs;[
-        python3
-    ];
-    programs.uv = {
-        enable = true;
-        settings = {
-            python-downloads = "never";
-            pip.index-url = "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple/";
-        };
+  home.packages = with pkgs;[
+    (python3.withPackages (ps: with ps; [
+      pandas
+      matplotlib
+      numpy
+      ipython
+      requests
+    ]))
+  ];
+  programs.uv = {
+    enable = true;
+    settings = {
+      python-downloads = "never";
+      pip.index-url = "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple/";
     };
+  };
 }
