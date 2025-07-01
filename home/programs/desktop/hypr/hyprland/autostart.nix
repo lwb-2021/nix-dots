015e77945b3a0b8ca8672e5b160f40db3269be26
lwb <lwb-2021@qq.com>
2025-07-01 17:42:58 +0800

    add calibre

--- home/programs/desktop/hypr/hyprland/autostart.nix
+++ home/programs/desktop/hypr/hyprland/autostart.nix
@@ -1,18 +1,19 @@
 { config, pkgs, lib, ... }: {
-    wayland.windowManager.hyprland = {
-        systemd = {
-            enable = true;
-            extraCommands = [
-                "systemctl --user stop hyprpolkitagent.service"
-                "systemctl --user stop hyprland-session.target"
-                "systemctl --user start hyprland-session.target"
-                "systemctl --user start hyprpolkitagent.service"
-            ];
-        };
-        settings.exec-once = [
-            "keepassxc"
-            "ags run"
-        ];
+  wayland.windowManager.hyprland = {
+    systemd = {
+      enable = true;
+      extraCommands = [
+        "systemctl --user stop hyprpolkitagent.service"
+        "systemctl --user stop hyprland-session.target"
+        "systemctl --user start hyprland-session.target"
+        "systemctl --user start hyprpolkitagent.service"
+      ];
     };
+    settings.exec-once = [
+      "keepassxc"
+      "calibre --start-in-tray"
+      "ags run"
+    ];
+  };
 
 }
