d9964206ee361678edd5d9b79db9066d7c692c00
lwb <lwb-2021@qq.com>
2025-07-02 12:18:34 +0800

    add cachix nix-community binary cache

--- configuration.nix
+++ configuration.nix
@@ -162,11 +162,13 @@
     ];
 
     extra-substituters = lib.mkForce [ 
+      "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://yazi.cachix.org"
       "https://devenv.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
