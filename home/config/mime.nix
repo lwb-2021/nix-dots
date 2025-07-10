2b70fa60c20d42e4581adcff426960ab0cd4676c
lwb <lwb-2021@qq.com>
2025-07-10 20:02:29 +0800

    enable xdg mimeApps and move firefox mime to firefox.nix

--- home/config/mime.nix
+++ home/config/mime.nix
@@ -1,26 +1,19 @@
 { config, lib, pkgs, ... }: {
-    xdg = {
-        mime.enable = true;
-        mimeApps = {
-            defaultApplications = {
-                "text/html" = "firefox.desktop";
-                "x-scheme-handler/http" = "firefox.desktop";
-                "x-scheme-handler/https" = "firefox.desktop";
-                "x-scheme-handler/about" = "firefox.desktop";
-                "x-scheme-handler/unknown" = "firefox.desktop";
-                "application/pdf" = "firefox.desktop";
+  xdg = {
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "audio/mpeg" = "mpv.desktop";
+        "audio/wav" = "mpv.desktop";
+        "video/mpeg" = "mpv.desktop";
+        "video/mp4" = "mpv.desktop";
 
-
-                "audio/mpeg" = "mpv.desktop";
-                "audio/wav" = "mpv.desktop";
-                "video/mpeg" = "mpv.desktop";
-                "video/mp4" = "mpv.desktop";
-
-                "text/plain" = "nvim.desktop";
-                "text/markdown" = "nvim.desktop";
-            };
-        };
+        "text/plain" = "nvim.desktop";
+        "text/markdown" = "nvim.desktop";
+      };
     };
+  };
 
 
 } 
--- home/programs/network/firefox.nix
+++ home/programs/network/firefox.nix
@@ -7,4 +7,12 @@
     };
     #profiles.default = {};
   };
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = "firefox.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+    "x-scheme-handler/about" = "firefox.desktop";
+    "x-scheme-handler/unknown" = "firefox.desktop";
+    "application/pdf" = "firefox.desktop";
+  };
 }
