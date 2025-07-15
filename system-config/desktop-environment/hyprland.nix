4398e73b73d8ca154a48590a1977fbc96b888802
lwb <lwb-2021@qq.com>
2025-07-15 18:10:32 +0800

    enable uwsm for hyprland

--- system-config/desktop-environment/hyprland.nix
+++ system-config/desktop-environment/hyprland.nix
@@ -1,15 +1,19 @@
 { config, pkgs, lib, inputs, ... }: {
-    environment.systemPackages = with pkgs;[
-        hyprpolkitagent
-    ];
-    programs.hyprland = {
-        enable = true;
-        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-        xwayland.enable = true;
-    };
-    security.polkit = {
-        enable = true;
-    };
-    security.pam.services.hyprlock = {};
+  environment.systemPackages = with pkgs;[
+    hyprpolkitagent
+  ];
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    xwayland.enable = true;
+  };
+  programs.uwsm = {
+    enable = true;
+  };
+  security.polkit = {
+    enable = true;
+  };
+  security.pam.services.hyprlock = {};
 }
