1a59780ecff39d985d120ebde855a7ac41757c8b
lwb <lwb-2021@qq.com>
2025-07-15 11:43:23 +0800

    add rainbow-delimiters

--- nixvim/plugins/default.nix
+++ nixvim/plugins/default.nix
@@ -1,16 +1,19 @@
 { pkgs, ... }: {
-    programs.nixvim.plugins = {
-        bufferline = import ./bufferline.nix;
+  programs.nixvim.plugins = {
+    bufferline = import ./bufferline.nix;
 
-        cmp = import ./cmp.nix;
+    cmp = import ./cmp.nix;
 
-        neogit = import ./neogit.nix;
+    neogit = import ./neogit.nix;
 
-        nvim-tree = import ./nvim-tree.nix;
-        treesitter = import ./treesitter.nix pkgs;
-        web-devicons.enable = true;
+    nvim-tree = import ./nvim-tree.nix;
 
-        which-key = import ./which-key.nix;
+    treesitter = import ./treesitter.nix pkgs;
+    web-devicons.enable = true;
 
-    };
+
+    rainbow-delimiters = import ./rainbow-delimiters.nix;
+
+    which-key = import ./which-key.nix;
+  };
 }
