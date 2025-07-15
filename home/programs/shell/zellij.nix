0f7904c4846d3977d8e525cbe6494d7dd7e936ec
lwb <lwb-2021@qq.com>
2025-07-15 19:50:50 +0800

    stop zellij from autostarting in vscode

--- home/programs/shell/zellij.nix
+++ home/programs/shell/zellij.nix
@@ -1,14 +1,21 @@
 { config, lib, pkgs, ... }: {
   programs.zellij = {
     enable = true;
-    attachExistingSession = true;
-    exitShellOnExit = false;
-    enableFishIntegration = true;
+    enableFishIntegration = false;
     settings = {
       theme = "tokyo-night-dark";
       show_startup_tips = false;    
       default_mode = "locked";
     };
   };
+  programs.fish.shellInitLast = /* fish */ ''
+if status is-interactive
+    and test "$TERM_PROGRAM" != "vscode"
+    and not set -q ZELLIJ
+
+    zellij attach -c default
+end
+
+  '';
 
 }
