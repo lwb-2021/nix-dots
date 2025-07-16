69a0f8ebb19290c584ada087a77a98b994380fc7
lwb <lwb-2021@qq.com>
2025-07-16 12:50:40 +0800

    update hyprland windowrule for waydroid

--- home/programs/desktop/hypr/hyprland/rules.nix
+++ home/programs/desktop/hypr/hyprland/rules.nix
@@ -1,14 +1,15 @@
 { config, lib, pkgs, ... }: {
-    wayland.windowManager.hyprland = {
-        settings = {
-            windowrule = [
-                "float, title:.*正在.*"
-                #"size 400 200, title:.*正在.*"
-                "move 100%-w-20 80, title:.*正在.*"
+  wayland.windowManager.hyprland = {
+    settings = {
+      windowrule = [
+        "float, title:.*正在.*"
+        #"size 400 200, title:.*正在.*"
+        "move 100%-w-20 80, title:.*正在.*"
 
-                "float, class:org.kde.kdeconnect.handler"
-                "center, class:org.kde.kdeconnect.handler"
-            ];
-        };
+        "float, class:org.kde.kdeconnect.handler"
+        "center, class:org.kde.kdeconnect.handler"
+        "float, class:(waydroid)(.*)"
+      ];
     };
+  };
 }
