a434d7e1f4801de5a0a7e2bbdbb805608f435ceb
lwb <lwb-2021@qq.com>
2025-07-17 09:52:11 +0800

    fixup! fixup! Reset: add nixpak to wechat

--- pkgs/wechat-pak.nix
+++ pkgs/wechat-pak.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, mkNixPak, ... }: (mkNixPak { config = { sloth, ... }: {
+{ lib, mkNixPak, ... }: (mkNixPak { config = { pkgs, sloth, ... }: {
   flatpak = {
     appId = "com.tencent.wechat";
   };
@@ -19,9 +19,6 @@
       (sloth.mkdir (sloth.concat [ sloth.xdgDocumentsDir "/WeChat" ]))
     ];
     bind.ro = [
-      "/etc/fonts"
-      "/etc/machine-id"
-      "/etc/localtime"
       "/etc/passwd"
       "/etc/ssl/certs/ca-bundle.crt"
       "/etc/ssl/certs/ca-certificates.crt"
@@ -29,45 +26,22 @@
     ];
     network = true;
     sockets = {
-      x11 = true;
+      #x11 = true;
       wayland = true;
       pipewire = true;
     };
     bind.dev = [
-      "/dev/dri"
       "/dev/shm"
       "/dev/video0"
       "/dev/video1"
     ];
-    tmpfs = [
-      "/tmp"
-    ];
     env = {
       IBUS_USE_PORTAL = "1";
-      XDG_DATA_DIRS = lib.mkForce (lib.makeSearchPath "share" (with pkgs; [
-
-        phinger-cursors
-        beauty-line-icon-theme
-        (graphite-gtk-theme.override {
-          themeVariants = [ "purple" ];
-          sizeVariants = [ "compact" ];
-          tweaks = [ "black" ];
-          wallpapers = true;
-          withGrub = true;
-        })
-
-        shared-mime-info
-      ]));
-      XCURSOR_PATH = lib.mkForce (lib.concatStringsSep ":" (with pkgs; [
-        "${phinger-cursors}/share/icons"
-        "${phinger-cursors}/share/pixmaps"
-        "${beauty-line-icon-theme}/share/icons"
-        "${beauty-line-icon-theme}/share/pixmaps"
-      ]));
     };
   };
   app = {
     package = pkgs.wechat;
   };
+  imports = [ ./base/nix-pak-gui.nix ];
 };
 }).config.script
