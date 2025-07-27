a5401b6f4a944a9df08b637eb9ae6caf45acb26a
lwb <lwb-2021@qq.com>
2025-07-27 12:06:23 +0800

    change mako width and height

--- home/programs/desktop/notification/mako.nix
+++ home/programs/desktop/notification/mako.nix
@@ -2,32 +2,32 @@
   services.mako = {
     enable = true;
     settings = {
-      sort="-time";
-      layer="overlay";
-      background-color="#2e34407f";
-      width=420;
-      height=120;
-      border-size=3;
-      border-color="#99c0d0";
-      border-radius=12;
-      icons=0;
-      max-icon-size=64;
-      default-timeout=5000;
-      ignore-timeout=0;
-      font="\"Source Han Sans CN\" 14";
-      margin=12;
-      padding="12,20";
+      sort = "-time";
+      layer = "overlay";
+      background-color = "#2e34407f";
+      width = 160;
+      height = 90;
+      border-size = 1;
+      border-color = "#99c0d0";
+      border-radius = 12;
+      icons = 0;
+      max-icon-size = 64;
+      default-timeout = 5000;
+      ignore-timeout = 0;
+      font="\"Source Han Sans CN\" 12";
+      margin = 12;
+      padding= "6, 10";
 
       "urgency=low" = {
-        border-color="#cccccc";
+        border-color = "#cccccc";
       };
       "urgency=normal" = {
-        border-color="#99c0d0";
+        border-color = "#99c0d0";
       };
 
       "urgency=critical" = {
-        border-color="#bf616a";
-        default-timeout=0;
+        border-color = "#bf616a";
+        default-timeout = 0;
       };
       
     };
