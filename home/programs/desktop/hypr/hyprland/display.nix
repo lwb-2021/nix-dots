f7d3b0df5e6fad8ab59d8d0f69c30dcb798aaef2
lwb <lwb-2021@qq.com>
2025-07-29 17:57:04 +0800

    add xauthority?

--- home/programs/desktop/hypr/hyprland/display.nix
+++ home/programs/desktop/hypr/hyprland/display.nix
@@ -1,17 +1,16 @@
 { config, pkgs, lib, ... }: {
-    home.packages = with pkgs;[xorg.xrdb];
-    wayland.windowManager.hyprland.settings = {
-        monitor = ", highres, auto, 1.6";
+  home.packages = with pkgs;[xorg.xrdb];
+  wayland.windowManager.hyprland.settings = {
+    monitor = ", highres, auto, 1.6";
 
-        # unscale XWayland
-        xwayland = {
-            force_zero_scaling = true;
-        };
+    xwayland = {
+      force_zero_scaling = true;
+      create_abstract_socket = true;
+    };
 
-        # toolkit-specific scale
-        exec-once = [
-            "echo 'Xft.dpi: 153.6' | xrdb -merge"
-        ];
+    exec-once = [
+      "cat ~/.Xresources | xrdb -merge"
+    ];
 
-    };   
+  };   
 }
