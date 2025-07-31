5fd71901b462f4e5a39771aa10e78d972c556c56
lwb <lwb-2021@qq.com>
2025-07-31 14:31:57 +0800

    fix ncmpcpp build failed

--- overlays/fix.nix
+++ overlays/fix.nix
@@ -1,12 +1,14 @@
 { lib, pkgs, inputs, ... }: {
   nixpkgs.overlays = [
     (final: prev: { 
-      graphite-gtk-theme = prev.callPackage (builtins.fetchurl 
-        {
-          url = "https://ghproxy.cn/raw.githubusercontent.com/romildo/nixpkgs/7875cddddd88c696039bf94b7c1e398a471f8d14/pkgs/by-name/gr/graphite-gtk-theme/package.nix";
+      graphite-gtk-theme = prev.callPackage (builtins.fetchurl {
+          url = "https://ghproxy.cn/https://github.com/NixOS/nixpkgs/raw/refs/heads/master/pkgs/by-name/gr/graphite-gtk-theme/package.nix";
           sha256 = "sha256:14k7090ksn5cx52fi3pxdm30jl51fihkjryf4ihhmbbqj4xa1b3l";
-        }
-      ) {};    
+      }) {};    
+      ncmpcpp = prev.callPackage (builtins.fetchurl{
+        url = "https://ghproxy.cn/https://github.com/NixOS/nixpkgs/raw/refs/heads/master/pkgs/by-name/nc/ncmpcpp/package.nix";
+        sha256 = "sha256:1cyn3alsda4kbr972fc317fyqnf1l5yii7nnbh48xr5bfn8zd9m8";
+      }) {};
     })
   ];
 }
