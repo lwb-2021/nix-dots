2e506d1127321d6d8560e043ab5c851397d6194b
lwb <lwb-2021@qq.com>
2025-08-08 17:26:17 +0800

    change anyrun settings

--- home/programs/desktop/apps/anyrun.nix
+++ home/programs/desktop/apps/anyrun.nix
@@ -4,20 +4,22 @@
     package = inputs.anyrun.packages.${pkgs.system}.anyrun;
     config = {
       x = { fraction = 0.5; };
-      y = { fraction = 0.3; };
-      width = { fraction = 0.4; };
+      y = { fraction = 0.4; };
+      width = { fraction = 0.35; };
       height = { fraction = 0.2;};
       hideIcons = false;
       ignoreExclusiveZones = false;
       layer = "overlay";
       hidePluginInfo = true;
       closeOnClick = false;
-      showResultsImmediately = false;
+      showResultsImmediately = true;
       maxEntries = null;
       plugins = with inputs.anyrun.packages.${pkgs.system};[
         # An array of all the plugins you want, which either can be paths to the .so files, or their packages
         applications
         kidex
+        shell
+        websearch
       ] ++ [
 
         ];
@@ -28,6 +30,28 @@
           * { background-color: unset; }
         }
     '';
+    extraConfigFiles = {
+      "websearch.ron".text = ''
+        Config(
+          prefix: "@",
+          engines: [
+            Bing, 
+            Custom(
+              name: "NixOS Packages",
+              url: "search.nixos.org/packages?channel=unstable&query={}",
+            ),
+            Custom(
+              name: "NixOS Options",
+              url: "search.nixos.org/options?channel=unstable&query={}",
+            ),
+          ], 
+        )
+      '';
+      "shell.ron".text = ''
+        prefix: "$",
+        shell: None,
+      '';
+    };
 
   };
 
