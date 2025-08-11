09ecc1bc409b9a37a23f861176421894c73dd134
lwb <lwb-2021@qq.com>
2025-08-11 10:00:44 +0800

    put firefox into nix

--- home/programs/network/firefox.nix
+++ home/programs/network/firefox.nix
@@ -9,17 +9,31 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4101199/minimalist_dark_and_purple_tab-3.0.xpi";
           private_browsing = true;
         };
+        "{e2488817-3d73-4013-850d-b66c5e42d505}" = {
+          enabled = true; 
+          initialize = true; 
+          menu = true; 
+          ua = true; 
+          windowLoc = true;
+          autoSet = true;
+          path = config.xdg.userDirs.download;
+          protocol = "ws";
+          host="127.0.0.1";
+          port="6800";
+          interf="jsonrpc";
+          token="";
+          sound="3";
+        };
       };
     };
-    profiles.default = {
-      id = 1;
-      name = "default";
-      isDefault = true;
-    };
-    profiles.lwb = {
-      name = "lwb";
+    profiles.default-bak = {
+      id = 1145;
       isDefault = false;
+    };
+    profiles.default = {
+      isDefault = true;
       extensions = {
+        force = true;
         packages = with pkgs.nur.repos.rycee.firefox-addons; [
           # Downloader
           aria2-integration
@@ -35,26 +49,77 @@
           keepassxc-browser
           zotero-connector
 
+          # Security
+          noscript
+
           # Improvements
           ublock-origin
           redirector
 
           tampermonkey # Script
         ];
+        settings = {
+          "{3c078156-979c-498b-8990-85f7987dd929}".settings = {
+            settings = {
+              navBarLayout = "hidden";
+
+              previewTabs = true;
+              previewTabsMode = "p";
+              previewTabsPageModeFallback = "n";
+
+              snapLimit = 3;
+              snapLimitUnit = "snap";
+
+              pinnedNoUnload = false;
+
+              syncUseFirefox = false;
+            };
+          };
+        };
       };
       settings = {
-
-        "app.normandy.first_run" = false;
-
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        # 基础设置
         "extensions.autoDisableScopes" = 0;
+        "extensions.activeThemeID" = "{32662a95-d1c0-40d9-b68b-aa682008c05c}";
 
         "font.language.group" = "zh-CN";
-        "intl.accept_languages" = "zh-CN, zh, zh-TW, zh-HK, en-US, en";
+        "intl.locale.requested" = "zh-CN,en-US";
 
+        # UI
         "browser.toolbars.bookmarks.visibility" = "always";
-        
+        "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["_3c078156-979c-498b-8990-85f7987dd929_-browser-action","back-button","forward-button","stop-reload-button","customizableui-special-spring1","vertical-spacer","urlbar-container","customizableui-special-spring2","downloads-button","fxa-toolbar-menu-button","unified-extensions-button","zotero_chnm_gmu_edu-browser-action"],"toolbar-menubar":["menubar-items"],"vertical-tabs":[],"PersonalToolbar":["personal-bookmarks"]},"dirtyAreaCache":["unified-extensions-area","nav-bar","vertical-tabs","PersonalToolbar"],"currentVersion":22,"newElementCount":3}'';
+
         "media.autoplay.default" = 5;
+
+        # 功能
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+        # 优化
+        "nglayout.initialpaint.delay" = 0;
+      };
+
+      search.default = "bing";
+      search.engines = {
+        nix-packages = {
+          name = "Nix Packages";
+          urls = [{
+            template = "https://search.nixos.org/packages";
+            params = [
+              { name = "type"; value = "packages"; }
+              { name = "query"; value = "{searchTerms}"; }
+            ];
+          }];
+
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" ];
+        };
+
+        nixos-wiki = {
+          name = "NixOS Wiki";
+          urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
+          iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+          definedAliases = [ "@nw" ];
+        };
       };
 
       userChrome = ''
