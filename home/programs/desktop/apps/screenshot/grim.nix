6b55978337c9e9f95dbfab4cbeff57287f66cbcb
lwb <lwb-2021@qq.com>
2025-08-19 21:30:41 +0800

    add screenshot shortcut

--- home/programs/desktop/apps/screenshot/grim.nix
+++ home/programs/desktop/apps/screenshot/grim.nix
@@ -1,7 +1,24 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   home.packages = with pkgs;[
     slurp
     grim
+    ksnip
   ];
-  wayland.screenshot.exec = "";
+  home.file.".config/scripts/screenshot.sh" = {
+    text = ''
+      #!/usr/bin/env bash
+      SCREENSHOT_DIR="$XDG_PICTURES_DIR/Screenshots"
+      mkdir -p "$SCREENSHOT_DIR"
+      TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+      FILE="$SCREENSHOT_DIR/screenshot_$TIMESTAMP.png"
+      grim -g "$(slurp)" "$FILE"
+      if [ $? -eq 0 ]; then
+        notify-send "截图完成" "已保存至 $FILE"
+        ksnip $FILE
+        wl-copy < $FILE
+      fi
+    '';
+    executable = true;
+  };
+  wayland.screenshot.exec = "~/.config/scripts/screenshot.sh";
 }
