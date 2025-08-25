b187d332f15975a3f15c70012257df451d3382b5
lwb <lwb-2021@qq.com>
2025-08-25 12:22:00 +0800

    add pandoc zotero filter

--- home/programs/daily/pandoc.nix
+++ home/programs/daily/pandoc.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
@@ -12,11 +11,19 @@
         author = "lwb";
       };
       pdf-engine = "xelatex";
-      citeproc = true;
+      filters = [
+        "${config.home.homeDirectory}/.config/pandoc-filters/zotero.lua"
+      ];
       variables = {
         mainfont = "Source Han Sans SC";
       };
 
+    };
+  };
+  home.file = {
+    ".config/pandoc-filters/zotero.lua".source = pkgs.fetchurl {
+      url = "https://retorque.re/zotero-better-bibtex/exporting/zotero.lua";
+      hash = "sha256-I0HLtoCNYY2Om2YOIUkZXsmakQtxhDMtiBlYDKUL6F0=";
     };
   };
 
