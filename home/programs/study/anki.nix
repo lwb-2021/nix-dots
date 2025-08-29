34c92e5cca6a77393dad580a53ea6dd0ebdaf96b
lwb <lwb-2021@qq.com>
2025-08-29 14:31:28 +0800

    fix update

--- home/programs/study/anki.nix
+++ home/programs/study/anki.nix
@@ -1,21 +1,23 @@
 { pkgs, ... }:
 {
-
-  programs.anki = {
-    enable = true;
-    package = pkgs.stable.anki; # TODO: move anki to unstable after fixed
-    addons = with pkgs.ankiAddons; [
-      anki-connect
-    ];
-    language = "zh_CN";
-    theme = "dark";
-    style = "native";
-    sync = {
-      autoSync = true;
-      autoSyncMediaMinutes = 120;
-      username = "lwb-2021@qq.com";
-      passwordFile = ./anki-pwd.txt;
-    };
-  };
+  home.packages = with pkgs; [
+    stable.anki
+  ];
+  # programs.anki = {
+  #   enable = true;
+  #   package = pkgs.stable.anki; # TODO: move anki to unstable after fixed
+  #   # addons = with pkgs.ankiAddons; [
+  #   #   anki-connect
+  #   # ];
+  #   language = "zh_CN";
+  #   theme = "dark";
+  #   style = "native";
+  #   sync = {
+  #     autoSync = true;
+  #     autoSyncMediaMinutes = 120;
+  #     username = "lwb-2021@qq.com";
+  #     passwordFile = ./anki-pwd.txt;
+  #   };
+  # };
 
 }
