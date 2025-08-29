23e0fa479d61a973c556c8543ed9843039cdafa9
lwb <lwb-2021@qq.com>
2025-08-29 10:06:15 +0800

    add wps

--- home/programs/daily/office.nix
+++ home/programs/daily/office.nix
@@ -1,13 +1,23 @@
 {
-  config,
-  lib,
   pkgs,
   ...
 }:
 {
   home.packages = with pkgs; [
     libreoffice-fresh
-    pkgs.jdk17
+    (wpsoffice-cn.overrideAttrs {
+      postInstall = ''
+        for i in $out/share/applications/*;do
+          sed -i '/Exec=/d' $i
+          echo Exec=\'env XMODIFIERS="@im=fcitx" GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE=fcitx GLFW_IM_MODULE=ibus QT_SCALE_FACTOR=1.5\' >> $i
+        done
+      '';
+    })
+    nur.repos.rewine.ttf-wps-fonts
   ];
+  programs.java = {
+    enable = true;
+    package = pkgs.openjdk21;
+  };
 
 }
