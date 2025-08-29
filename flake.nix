94b2adc4cceb1bf4543368503769ae0c48d63a52
lwb <lwb-2021@qq.com>
2025-08-29 10:48:52 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -36,6 +36,11 @@
       url = "github:nix-community/nh";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
