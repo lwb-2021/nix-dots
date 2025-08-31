2dd9239eedc2f05edf5b968dc2e321f3fdfce539
lwb <lwb-2021@qq.com>
2025-08-31 09:42:19 +0800

    try to fix office

--- home/programs/daily/office.nix
+++ home/programs/daily/office.nix
@@ -8,7 +8,7 @@
     (wpsoffice-cn.overrideAttrs {
       postInstall = ''
         for i in $out/share/applications/*;do
-          echo Exec=\'env XMODIFIERS="@im=fcitx" GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE=fcitx GLFW_IM_MODULE=ibus QT_SCALE_FACTOR=1.5\' $(sed -n 's/^Exec=//p') >> $i
+          echo Exec=\'env XMODIFIERS="@im=fcitx" GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE=fcitx GLFW_IM_MODULE=ibus QT_SCALE_FACTOR=1.5 $(sed -n 's/^Exec=//p' $i)\' >> $i
           sed -i '0,/^Exec=/ {/^Exec=/d}' $i
         done
       '';
