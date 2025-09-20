2a01feb94f95d3e97c1dc379f1b9a55c5ce0ec06
lwb <lwb-2021@qq.com>
2025-09-20 19:46:40 +0800

    add permissions

--- home/programs/desktop/hypr/hyprland/permissions.nix
+++ home/programs/desktop/hypr/hyprland/permissions.nix
@@ -1,11 +1,13 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
 
   wayland.windowManager.hyprland.settings = {
-    ecosystem.enforce_permission = true;
+    ecosystem.enforce_permissions = true;
     permission = [
-      # "grim, screencopy, allow"
-      ""
+      "${lib.escapeRegex (lib.getExe pkgs.grim)}, screencopy, allow"
+      "${lib.escapeRegex (lib.getExe pkgs.hyprlock)}, screencopy, allow"
+      "${lib.escapeRegex "${pkgs.xdg-desktop-portal-hyprland}"}/libexec/.xdg-desktop-portal-hyprland-wrapped, screencopy, allow"
+      "${lib.escapeRegex "${pkgs.hyprland}"}/bin/hyprctl, plugin, allow"
     ];
   };
 
