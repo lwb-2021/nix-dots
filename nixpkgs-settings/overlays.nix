1265399bfe9c98e03e4bd12e5daa99d7b7971b9d
lwb <lwb-2021@qq.com>
2025-09-20 23:28:12 +0800

    update beautyline to 2.4

--- nixpkgs-settings/overlays.nix
+++ nixpkgs-settings/overlays.nix
@@ -30,6 +30,16 @@
         hash = "sha256-gBWcNQ1o1AZfNsmu1Vi1Kilqv3YbR+wqOod4XYAeVKo=";
       };
     };
+    beauty-line-icon-theme = prev.beauty-line-icon-theme.overrideAttrs rec {
+      version = "2.4";
+      src = prev.fetchFromGitLab {
+        owner = "garuda-linux";
+        repo = "themes-and-settings/artwork/beautyline";
+        rev = "0df6f5df71c19496f9a873f8a52fbb5e84e95b12";
+        hash = "sha256-SsYW4H1qam7kQJ3E4/vHJJOv2E4Pdk3itGncWa6YTqw=";
+      };
+      sourceRoot = "${src.name}"; # 相当于删除源文件的该属性
+    };
   })
   (final: prev: {
   })
