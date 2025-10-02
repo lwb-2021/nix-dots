2eb4da4f0b2a386e54acd831da4fc134ca9d1c01
lwb <lwb-2021@qq.com>
2025-10-02 22:25:23 +0800

    add waydroid-helper

--- system-config/software/apps/android.nix
+++ system-config/software/apps/android.nix
@@ -3,6 +3,7 @@
   programs.adb.enable = true;
   users.users.lwb.extraGroups = [ "adbusers" ];
   virtualisation.waydroid.enable = true;
+  environment.systemPackages = with pkgs; [ waydroid-helper ];
   services.udev.extraRules = ''
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="05c6", ATTRS{idProduct}=="9006", MODE="0666", GROUP="plugdev"
 
