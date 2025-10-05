dcafb0df4e631254666a176109e5b031dd7cc603
lwb <lwb-2021@qq.com>
2025-10-05 18:40:09 +0800

    change neovide config

--- home/programs/development/editors/neovim.nix
+++ home/programs/development/editors/neovim.nix
@@ -1,19 +1,46 @@
 { pkgs, ... }:
-{
-  home.packages = [
-    (pkgs.buildFHSEnv {
-      name = "nvim";
-      targetPkgs = (
-        pkgs: with pkgs; [
-          stdenv.cc
-          neovim-unwrapped
-          unzip
-          luarocks
-          wl-clipboard
-        ]
-      );
-      runScript = "nvim";
-    })
+let neovim = (pkgs.buildFHSEnv {
+  name = "nvim";
+  targetPkgs = (
+    pkgs: with pkgs; [
+      stdenv.cc
+      neovim-unwrapped
+      unzip
+      lua
+      luarocks
+      nodejs
+      cargo
 
+      tree-sitter
+
+      wl-clipboard
+    ]
+  );
+  runScript = "nvim";
+});
+in
+  {
+
+  home.packages = [
+    neovim
+    pkgs.lazygit
   ];
+  programs.neovide = {
+    enable = true;
+    package = pkgs.neovide.override { inherit neovim; };
+    settings = {
+      maximized = true;
+      font = {
+        normal = [
+          {
+            family = "JetBrainsMono Nerd Font";
+          }
+        ];
+        size = 12.0;
+      };
+      box-drawing = {
+        mode = "native";
+      };
+    };
+  };
 }
