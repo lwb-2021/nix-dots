1bb9db5f96570bdd239c863259853e73078d6a22
lwb <lwb-2021@qq.com>
2025-10-11 22:29:48 +0800

    add glfw3 to prism launcher

--- home/programs/game/minecraft.nix
+++ home/programs/game/minecraft.nix
@@ -3,7 +3,10 @@
   home.packages = with pkgs; [
     (prismlauncher.override {
       # Add binary required by some mod
-      additionalPrograms = [ ffmpeg ];
+      additionalPrograms = [
+        ffmpeg
+        glfw3
+      ];
 
       # Change Java runtimes available to Prism Launcher
       jdks = [
