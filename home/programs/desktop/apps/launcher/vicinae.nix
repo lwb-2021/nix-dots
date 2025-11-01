cfebca4bf2eae92a9d98082b8d674c67888a1695
lwb <lwb-2021@qq.com>
2025-11-01 13:48:06 +0800

    vicinae: basic config

--- home/programs/desktop/apps/launcher/vicinae.nix
+++ home/programs/desktop/apps/launcher/vicinae.nix
@@ -3,6 +3,16 @@
   services.vicinae = {
     enable = true;
     autoStart = true;
+    settings = {
+      theme.name = "vicinae-dark";
+      window = {
+        csd = true;
+        opacity = 0.9;
+        rounding = 10;
+      };
+
+      faviconService = "twenty";
+    };
   };
   wayland.launcher.exec = "vicinae toggle";
 }
