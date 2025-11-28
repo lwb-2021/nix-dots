30b5495e1c707e5c83ab164781e89c959fe0b817
lwb <lwb-2021@qq.com>
2025-11-28 23:31:24 +0800

    niri: fix border radius

--- home/programs/desktop/niri/rules.nix
+++ home/programs/desktop/niri/rules.nix
@@ -2,7 +2,16 @@
 {
   programs.niri.settings.window-rules = [
     {
-      geometry-corner-radius = 12;
+      geometry-corner-radius =
+        let
+          radius = 12.0;
+        in
+        {
+          bottom-left = radius;
+          bottom-right = radius;
+          top-left = radius;
+          top-right = radius;
+        };
       clip-to-geometry = true;
     }
   ];
