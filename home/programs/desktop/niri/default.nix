860e732e0dc5af4967635a7dde84a039ed7faf25
lwb <lwb-2021@qq.com>
2025-11-28 19:43:54 +0800

    niri: disable natural scroll

--- home/programs/desktop/niri/default.nix
+++ home/programs/desktop/niri/default.nix
@@ -8,6 +8,11 @@
     enable = true;
     package = pkgs.niri;
     settings = {
+      input = {
+        touchpad = {
+          natural-scroll = false;
+        };
+      };
       spawn-at-startup = [
         { argv = [ "~/.config/autostart.sh" ]; }
         { argv = [ "${lib.getExe pkgs.xwayland-satellite}" ]; }
