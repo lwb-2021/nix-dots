04b561712c8e9f9e58bb7224763d7419927d89a4
lwb <lwb-2021@qq.com>
2025-12-13 14:35:30 +0800

    regreet: enable theme with nix module

--- system-config/desktop-environment/dm.nix
+++ system-config/desktop-environment/dm.nix
@@ -1,25 +1,36 @@
-{
-  lib,
-  ...
-}:
+{ pkgs, ... }:
 {
   programs.regreet = {
     enable = true;
+    theme = {
+      name = "catppuccin-mocha-mauve-compact+black";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "mauve" ];
+        size = "compact";
+        tweaks = [ "black" ];
+        variant = "mocha";
+      };
+    };
+    cursorTheme = {
+      name = "phinger-cursors-dark";
+      package = pkgs.phinger-cursors;
+    };
+    iconTheme = {
+      name = "BeautyLine";
+      package = pkgs.beauty-line-icon-theme;
+    };
     settings = {
       GTK = {
         application_prefer_dark_theme = true;
 
         # Cursor theme name
-        cursor_theme_name = lib.mkForce "phinger-cursors-dark";
 
         # Font name and size
         #font_name = "Cantarell 16";
 
         # Icon theme name
-        icon_theme_name = lib.mkForce "BeautyLine";
 
         # GTK theme name
-        theme_name = lib.mkForce "catppuccin-mocha-mauve-compact+black";
       };
     };
   };
--- system-config/desktop-environment/theme.nix
+++ system-config/desktop-environment/theme.nix
@@ -1,24 +1,24 @@
 {
-  config,
-  lib,
   pkgs,
   ...
 }:
 {
-  environment.systemPackages = with pkgs; [
-    phinger-cursors
-    beauty-line-icon-theme
-    (catppuccin-gtk.override {
-      accents = [ "mauve" ];
-      size = "compact";
-      tweaks = [ "black" ];
-      variant = "mocha";
-    })
-  ];
-  catppuccin = {
-    enable = true;
-    accent = "mauve";
-    flavor = "mocha";
-  };
-
+  /*
+    environment.systemPackages = with pkgs; [
+      phinger-cursors
+      beauty-line-icon-theme
+      (catppuccin-gtk.override {
+        accents = [ "mauve" ];
+        size = "compact";
+        tweaks = [ "black" ];
+        variant = "mocha";
+      })
+    ];
+    catppuccin = {
+      enable = true;
+      accent = "mauve";
+      flavor = "mocha";
+    };
+  */
+  catppuccin.cache.enable = true;
 }
