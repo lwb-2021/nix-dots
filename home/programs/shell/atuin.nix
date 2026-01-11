497e02ae3858eebedb32a3251644e3fe5884b3ef
lwb <lwb-2021@qq.com>
2026-01-11 12:42:49 +0800

    atuin: change config

--- home/programs/shell/atuin.nix
+++ home/programs/shell/atuin.nix
@@ -7,6 +7,20 @@
       enable = true;
     };
     flags = [ "--disable-up-arrow" ];
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      history_filter = [
+        "^z\s+"
+        ".*/nix/store/.+"
+        "^python \./"
+      ];
+      store_failed = false;
+      secrets_filter = true;
 
+      sync = {
+        records = true;
+      };
+    };
   };
 }
