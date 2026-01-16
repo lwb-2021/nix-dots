95aea4d8efc982360b4a3feeda09356a7759014c
lwb <lwb-2021@qq.com>
2026-01-16 23:09:51 +0800

    rclone: move to a new file

--- home/data/default.nix
+++ home/data/default.nix
@@ -8,6 +8,7 @@
 
   imports = [
     ./home-data-preclude.nix
+    ./rclone.nix
   ];
 
   options = with lib; {
@@ -58,52 +59,6 @@
         files = [
         ]
         ++ config.data.local.files;
-      };
-    };
-
-    programs.rclone = {
-      enable = true;
-      remotes = {
-        remote = {
-          config = {
-            type = "compress";
-            compress-mode = "zstd";
-            remote = "remote-encrypted:/";
-          };
-          mounts = {
-            "" = {
-              mountPoint = "${config.home.homeDirectory}/Cloud";
-              options = {
-                fuse-flag = "x-gvfs-hide";
-              };
-            };
-          };
-        };
-
-        remote-encrypted = {
-          config = {
-            type = "crypt";
-            remote = "remote-raw:EncryptedData";
-          };
-          secrets.password = config.sops.secrets."rclone/crypt/password".path;
-        };
-        remote-raw = {
-          config = {
-            # type = "union";
-            # upstreams = "jianguoyun:/";
-            type = "alias";
-            remote = "jianguoyun:/";
-          };
-        };
-        jianguoyun = {
-          config = {
-            type = "webdav";
-            url = "https://dav.jianguoyun.com/dav/";
-            user = "lwb-2021@qq.com";
-            vendor = "other";
-          };
-          secrets.pass = config.sops.secrets."rclone/jianguoyun/password".path;
-        };
       };
     };
 
