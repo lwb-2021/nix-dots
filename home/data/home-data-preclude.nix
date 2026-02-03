527f957e81a248ba9e78ebd585fc1ed454c66c89
lwb <lwb-2021@qq.com>
2026-02-03 22:43:42 +0800

    steam: enable persist

--- home/data/home-data-preclude.nix
+++ home/data/home-data-preclude.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }@params:
 {
   data = {
     directories = [
@@ -6,11 +6,18 @@
     ];
     files = [ ];
     local = {
-      directories = [
-        ".cache/fontconfig"
-        ".cache/mesa_shader_cache"
-        ".cache/nix"
-        ".cache/nvidia"
+      directories = lib.mkMerge [
+        [
+          ".cache/fontconfig"
+          ".cache/mesa_shader_cache"
+          ".cache/nix"
+          ".cache/nvidia"
+
+        ]
+        (lib.mkIf (params ? osConfig && params.osConfig.programs.steam.enable) [
+          ".steam"
+          ".local/share/Steam"
+        ])
       ];
       files = [ ];
     };
